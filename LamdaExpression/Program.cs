536f57fd5f346825c6ec3286671050d31913ce7e
Rajeshwarikota <kotarajeswari003.com>
2023-04-06 17:22:54 +0530

    [Rajeshwari kota] Adding Student Details

--- LamdaExpression/Program.cs
+++ LamdaExpression/Program.cs
@@ -11,7 +11,27 @@ namespace LamdaExpression
         static void Main(string[] args)
         {
             Console.WriteLine("....Lamda Expression....");
+
+            //Collection initializer
+            List<Student> list = new List<Student>()
+            {
+                //object initializer
+                new Student(){Id=1,Name="Raji",Age=22,Address="Hyderabad",PhoneNumber=9988776655},
+                new Student(){Id=2,Name="Ammu",Age=21,Address="chennai",PhoneNumber=9988776657},
+                new Student(){Id=3,Name="Neetha",Age=32,Address="Bangalore",PhoneNumber=9988775655},
+                new Student(){Id=4,Name="Nithya",Age=25,Address="vizag",PhoneNumber=9988775655},
+                new Student(){Id=5,Name="Teja",Age=23,Address="mumbai",PhoneNumber=9988776345},
+            };
+            DisplayStudentDetails(list);
             Console.ReadLine();
+        }
+        public static void DisplayStudentDetails(List<Student> list)
+        {
+            Console.WriteLine("Displaying All Students");
+            foreach(Student student in list)
+            {
+                Console.WriteLine(student);
+            }
         }
     }
 }
---
+++ LamdaExpression/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamdaExpression
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public long PhoneNumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id:{Id}  Name:{Name}  Age:{Age}  Address:{Address}  Phonenumber:{PhoneNumber}";
+        }
+    }
+}
